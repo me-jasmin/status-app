80067f0a3f836463f5fd946c28b5d4bd9c1b9c3e
Jasmin Memisevic <jasmin.memisevic@gmail.com>
2019-12-20 08:24:59 +0100

    entry point clean-up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Main from './main';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
 serviceWorker.unregister();
