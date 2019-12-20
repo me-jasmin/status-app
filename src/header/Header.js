226c1ccc1166d10d5141d03e323c62ee834638d9
Jasmin Memisevic <jasmin.memisevic@gmail.com>
2019-12-20 08:21:47 +0100

    header component

---
+++ src/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// images
+import logo from '../static/images/viskan_logo.svg';
+// styles
+import './header.styles.scss';
+
+const Header = () => {
+    return (
+        <header id="header">
+            <div id="main-logo">
+                <img src={logo} alt="Viskan" />
+            </div>
+            <div id="main-logo__status-text">Status</div>
+        </header>
+    );
+};
+
+export default Header;
