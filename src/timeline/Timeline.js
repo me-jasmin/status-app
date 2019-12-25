d9ef1c4940ce79dd21cd0055c4f37269be838129
Jasmin Memisevic <jasmin.memisevic@gmail.com>
2019-12-25 09:04:18 +0100

    timeline component added

---
+++ src/timeline/Timeline.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// styles
+import './timeline.styles.scss';
+
+const Timeline = props => {
+    const { events, title } = props;
+
+    console.log(events, title);
+
+    const renderTimeline = () => {
+        return events.map((event, index) => {
+            return (
+                <div className="timeline__event" key={index}>
+                    <div className="timeline__time">{event.time}</div>
+                    <div className="timeline__text">{event.text}</div>
+                </div>
+            );
+        });
+    };
+
+    return (
+        <div className="timeline__event__group__container">
+            {title && <div className="timeline__event__title">{title}</div>}
+            {renderTimeline()}
+        </div>
+    );
+};
+
+export default Timeline;
