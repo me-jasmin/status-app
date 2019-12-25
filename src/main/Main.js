0a4b8969309a9a93de978ef032651815034815d7
Jasmin Memisevic <jasmin.memisevic@gmail.com>
2019-12-25 09:03:49 +0100

    mock api and axios added

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,128 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Header from '../header';
 import GeneralStatus from '../general-status';
-import { Accordion } from '../accordion/';
+import CurrentEvents from '../current-events';
+import PastEvents from '../past-events';
 
+//styles
 import './main.styles.scss';
 
-export default class Main extends React.Component {
-    getAPIdata = {
-        status: {
-            general_status: 'no_issues',
-            general_status_msg: 'Everything is up and running',
-        },
-        data: [
-            {
-                key: 'database',
-                status: 'no_issues',
-                title: 'Database',
-                events: [
-                    {
-                        time: '2007-03-01T13:00:00+02:00',
-                        event_status: 'maintenance',
-                        event_title: 'Maintenance',
-                        event_text:
-                            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus sint provident vel ab reprehenderit cum soluta, suscipit facere nisi sed earum repellendus fuga debitis, nam molestiae minima voluptates possimus.',
-                    },
-                    {
-                        time: '2007-03-01T13:00:00+02:00',
-                        event_status: 'maintenance',
-                        event_title: 'Maintenance',
-                        event_text:
-                            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus sint provident vel ab reprehenderit cum soluta, suscipit facere nisi sed earum repellendus fuga debitis, nam molestiae minima voluptates possimus.',
-                    },
-                    {
-                        time: '2007-03-01T13:00:00+02:00',
-                        event_status: 'maintenance',
-                        event_title: 'Maintenance',
-                        event_text:
-                            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus sint provident vel ab reprehenderit cum soluta, suscipit facere nisi sed earum repellendus fuga debitis, nam molestiae minima voluptates possimus.',
-                    },
-                ],
-            },
-            {
-                key: 'citrix',
-                status: 'maintenance',
-                title: 'Citrix',
-                events: [
-                    {
-                        time: '2007-03-01T13:00:00+02:00',
-                        event_status: 'no_issues',
-                        event_title: 'No issues',
-                        event_text:
-                            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus sint provident vel ab reprehenderit cum soluta, suscipit facere nisi sed earum repellendus fuga debitis, nam molestiae minima voluptates possimus.',
-                    },
-                    {
-                        time: '2007-03-01T13:00:00+02:00',
-                        event_status: 'no_issues',
-                        event_title: 'No issues',
-                        event_text:
-                            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus sint provident vel ab reprehenderit cum soluta, suscipit facere nisi sed earum repellendus fuga debitis, nam molestiae minima voluptates possimus.',
-                    },
-                    {
-                        time: '2007-03-01T13:00:00+02:00',
-                        event_status: 'no_issues',
-                        event_title: 'No issues',
-                        event_text:
-                            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus sint provident vel ab reprehenderit cum soluta, suscipit facere nisi sed earum repellendus fuga debitis, nam molestiae minima voluptates possimus.',
-                    },
-                ],
-            },
-            {
-                key: 'integrations',
-                status: 'incident',
-                title: 'Integrations',
-                events: [
-                    {
-                        time: '2007-03-01T13:00:00+02:00',
-                        event_status: 'no_issues',
-                        event_title: 'No issues',
-                        event_text:
-                            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus sint provident vel ab reprehenderit cum soluta, suscipit facere nisi sed earum repellendus fuga debitis, nam molestiae minima voluptates possimus.',
-                    },
-                    {
-                        time: '2007-03-01T13:00:00+02:00',
-                        event_status: 'no_issues',
-                        event_title: 'No issues',
-                        event_text:
-                            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus sint provident vel ab reprehenderit cum soluta, suscipit facere nisi sed earum repellendus fuga debitis, nam molestiae minima voluptates possimus.',
-                    },
-                ],
-            },
-            {
-                key: 'web_shops',
-                status: 'outage',
-                title: 'Web shops',
-                events: [
-                    {
-                        time: '2007-03-01T13:00:00+02:00',
-                        event_status: 'no_issues',
-                        event_title: 'No issues',
-                        event_text:
-                            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus sint provident vel ab reprehenderit cum soluta, suscipit facere nisi sed earum repellendus fuga debitis, nam molestiae minima voluptates possimus.',
-                    },
-                    {
-                        time: '2007-03-01T13:00:00+02:00',
-                        event_status: 'no_issues',
-                        event_title: 'No issues',
-                        event_text:
-                            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus sint provident vel ab reprehenderit cum soluta, suscipit facere nisi sed earum repellendus fuga debitis, nam molestiae minima voluptates possimus.',
-                    },
-                ],
-            },
-        ],
-    };
+const Main = props => {
+    const [state, setState] = useState({
+        data: {},
+        isLoading: true
+    });
 
-    render() {
-        return (
-            <>
-                <Header />
-                <div className="page-wrapper">
-                    <GeneralStatus />
-                    <Accordion data={this.getAPIdata.data} />
-                </div>
-            </>
-        );
-    }
-}
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios('testAPI.json');
+            setState({
+                data: result.data,
+                isLoading: false
+            });
+        };
+        fetchData();
+    }, []);
+
+    if (state.isLoading) return null;
+
+    return (
+        <>
+            <Header />
+            <div className="page-wrapper">
+                <GeneralStatus data={state.data.status} />
+                <CurrentEvents data={state.data.current_events} />
+                <PastEvents data={state.data.past_events} />
+            </div>
+        </>
+    );
+};
+
+export default Main;
